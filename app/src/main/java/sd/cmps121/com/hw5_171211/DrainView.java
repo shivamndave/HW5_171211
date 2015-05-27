e9862067ff25433dcdda64f8f3465f49e4e4c0ca
Shivam Dave <sdave@ucsc.edu>
2015-05-26 22:35:39 -0700

    Moves the tomato object based on the accelerometer, adds a getter and setter to drainview which is used to set the accelerator changes and get those numbers to adjust the placement fo the tomato

--- app/src/main/java/sd/cmps121/com/hw5_171211/DrainView.java
+++ app/src/main/java/sd/cmps121/com/hw5_171211/DrainView.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
 
 /**
  * Created by shivamndave on 5/26/15.
@@ -18,6 +19,27 @@ public class DrainView extends View {
     private Paint line1Paint;
     private Paint line2Paint;
 
+    private int _tomatoStartX;
+    private int _tomatoStartY;
+
+    private float _xLoc;
+    private float _yLoc;
+
+    private float getTomatoX() {
+        return _xLoc;
+    }
+
+    private float getTomatoY() {
+        return _yLoc;
+    }
+
+    public void setTomatoLoc(float xLoc, float yLoc) {
+        _xLoc = xLoc;
+        _yLoc = yLoc;
+        sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED);
+        this.invalidate();
+    }
+
     public DrainView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initDrainView();
@@ -28,10 +50,16 @@ public class DrainView extends View {
 
         Resources r = this.getResources();
 
+        int mMeasuredWidth = getMeasuredWidth();
+        int mMeasuredHeight = getMeasuredHeight();
+
         tomatoPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         tomatoPaint.setColor(r.getColor(R.color.tomato_color));
         tomatoPaint.setStrokeWidth(1);
         tomatoPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        _tomatoStartX = mMeasuredWidth / 2;
+        _tomatoStartY = mMeasuredHeight / 8;
 
         drainPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         drainPaint.setColor(r.getColor(R.color.drain_color));
@@ -40,12 +68,12 @@ public class DrainView extends View {
 
         line1Paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         line1Paint.setColor(r.getColor(R.color.line_color));
-        line1Paint.setStrokeWidth(1);
+        line1Paint.setStrokeWidth(4);
         line1Paint.setStyle(Paint.Style.FILL_AND_STROKE);
 
         line2Paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         line2Paint.setColor(r.getColor(R.color.line_color));
-        line2Paint.setStrokeWidth(1);
+        line2Paint.setStrokeWidth(4);
         line2Paint.setStyle(Paint.Style.FILL_AND_STROKE);
     }
 
@@ -54,26 +82,26 @@ public class DrainView extends View {
         int mMeasuredWidth = getMeasuredWidth();
         int mMeasuredHeight = getMeasuredHeight();
 
-        int tomatoStartX = mMeasuredWidth / 2;
-        int tomatoStartY = mMeasuredHeight / 8 ;
-
         int drainX = mMeasuredWidth / 2;
-        int drainY = mMeasuredHeight - (mMeasuredHeight / 8) ;
+        int drainY = mMeasuredHeight - (mMeasuredHeight / 8);
+
+        _tomatoStartX += getTomatoX();
+        _tomatoStartY += getTomatoY();
 
         int line1StartX = -100;
-        int line1StartY = mMeasuredHeight / 4 ;
+        int line1StartY = mMeasuredHeight / 4;
         int line1EndX = mMeasuredWidth - (mMeasuredWidth / 3);
-        int line1EndY = mMeasuredHeight / 4 ;
+        int line1EndY = mMeasuredHeight / 4;
 
         int line2StartX = mMeasuredWidth;
         int line2StartY = mMeasuredHeight - (mMeasuredHeight / 4);
         int line2EndX = mMeasuredWidth / 3;
-        int line2EndY = mMeasuredHeight - (mMeasuredHeight / 4) ;
+        int line2EndY = mMeasuredHeight - (mMeasuredHeight / 4);
 
         int tomatoRadius = 50;
         int drainRadius = 75;
 
-        canvas.drawCircle(tomatoStartX, tomatoStartY, tomatoRadius, tomatoPaint);
+        canvas.drawCircle(_tomatoStartX, _tomatoStartY, tomatoRadius, tomatoPaint);
         canvas.drawCircle(drainX, drainY, drainRadius, drainPaint);
         canvas.drawLine(line1StartX, line1StartY, line1EndX, line1EndY, line1Paint);
         canvas.drawLine(line2StartX, line2StartY, line2EndX, line2EndY, line2Paint);
--- app/src/main/java/sd/cmps121/com/hw5_171211/MainActivity.java
+++ app/src/main/java/sd/cmps121/com/hw5_171211/MainActivity.java
@@ -2,16 +2,12 @@ package sd.cmps121.com.hw5_171211;
 
 import android.app.Activity;
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
-
-
-import sd.cmps121.com.hw5_171211.R;
+import android.util.Log;
 
 
 public class MainActivity extends Activity {
@@ -24,26 +20,33 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        _mSensorBool = setSensorManager();
-
+        _mSensorBool = setSensorManager();
     }
 
-//    private Boolean setSensorManager() {
-//        return ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).registerListener(
-//                new SensorEventListener() {
-//                    @Override
-//                    public void onSensorChanged(SensorEvent event) {
-//                        // I hope I got the signs right.  If not, experiment with this.
-//                        _accelx = -event.values[0];
-//                        _accely = event.values[1];
-//                    }
-//
-//                    @Override
-//                    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-//                    } //ignore
-//                },
-//                ((SensorManager) getSystemService(Context.SENSOR_SERVICE))
-//                        .getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_GAME);
-//    }
+    private Boolean setSensorManager() {
+        return ((SensorManager) getSystemService(Context.SENSOR_SERVICE)).registerListener(
+                new SensorEventListener() {
+                    @Override
+                    public void onSensorChanged(SensorEvent event) {
+                        // I hope I got the signs right.  If not, experiment with this.
+                        _accelx = -event.values[0];
+                        _accely = event.values[1];
+
+                        DrainView drain = (DrainView) findViewById(R.id.drainView);
+
+                        drain.setTomatoLoc(_accelx, _accely);
+
+                        Log.e("acc", Float.toString(_accelx));
+                        Log.e("acc", Float.toString(_accely));
+
+                    }
+
+                    @Override
+                    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+                    } //ignore
+                },
+                ((SensorManager) getSystemService(Context.SENSOR_SERVICE))
+                        .getSensorList(Sensor.TYPE_ACCELEROMETER).get(0), SensorManager.SENSOR_DELAY_GAME);
+    }
 
 }
